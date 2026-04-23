8c981a8adf8ca8699215361a16dfc4dd03df99e9
Suman Nayak <112371795+sumanNayak29@users.noreply.github.com>
2026-04-23 09:06:05 +0530

    fix:

--- doc-stetho/src/LoginPage.tsx
+++ doc-stetho/src/LoginPage.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './LoginPage.css'
+import MailIcon from './assets/icons/MailIcon'
 
 function LoginPage() {
   const [email, setEmail] = useState('')
@@ -116,9 +117,7 @@ function LoginPage() {
               <label htmlFor="email" className="form-label">Email Address</label>
               <div className="input-wrapper">
                 <span className="input-icon">
-                  <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                    <path d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-                  </svg>
+                  <MailIcon />
                 </span>
                 <input
                   id="email"
